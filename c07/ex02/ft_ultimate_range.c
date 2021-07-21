bc76c7c23bc5ee0afe1e9ba0ade87cfb5a14e970
Jules Laurent <jlaurent@c1r3s4.42lausanne.ch>
2021-07-21 17:38:00 +0200

    c07

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: jlaurent <jlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 16:46:26 by jlaurent          #+#    #+#             */
-/*   Updated: 2021/07/21 17:11:01 by jlaurent         ###   ########.fr       */
+/*   Updated: 2021/07/21 17:37:44 by jlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	*ft_range(int min, int max)
 	c = 0;
 	n = max - min;
 	if (min >= max)
-		return (0);
+		return (NULL);
 	tab = malloc(n * sizeof(int));
 	while (min < max)
 	{
@@ -36,5 +36,7 @@ int	ft_ultimate_range(int **range, int min, int max)
 {
 	*range = ft_range(min, max);
 
-	if (*range)
+	if (*range == NULL)
+		return (0);
+	return (max - min);
 }
