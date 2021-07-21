cd447bae5d269b06df287495cb8423299d9ef6a6
porgito <86039875+porgito@users.noreply.github.com>
2021-07-21 17:36:05 +0200

    Update r_dwssap.sh

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '1d' | sed n\;d | cut -d ':' -f1 | sed '/#/d' | rev | sort -nr | xargs
+cat /etc/passwd | sed '1d' | sed n\;d | cut -d ':' -f1 | sed '/#/d' | rev | sort -nr | sed -n "$FT_LINE1,$FT_LINE2 p" |  xargs | sed s/' '/', '/g | tr '\n' '.'
